600c97a121c08e3dc4566def94e03e8d697582d8
nitin0409sep <nitin5september@gmail.com>
2024-07-13 20:42:44 +0530

    Modified controllers of admin, subadmin and users, and removed code for creatinng restaurant and dishes by user

--- routes/users/users.route.ts
+++ routes/users/users.route.ts
@@ -1,7 +1,15 @@
 import { Router } from "express";
 import { authenticateUser } from "../../middlewares/authorization.auth.middleware";
 import { addAddressOfUser } from '../../controllers/users/user.controller';
+import { getAllRestaurants } from '../../controllers/common/restaurant.controller';
+import { getAllDish } from '../../controllers/common/dish.controller';
 export const userRoute = Router();
+
+// GET ALL RESTAURANT'S CREATED BY USER
+userRoute.get('/getAllRestaurant', [authenticateUser], getAllRestaurants)
+
+// GET ALL DISHES CREATED BY USER
+userRoute.get('/getAllDishes', [authenticateUser], getAllDish);
 
 // ADD USER ADDRESS
 userRoute.post('/addAddress', [authenticateUser], addAddressOfUser);
